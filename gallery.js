1d039e21bf2d1dfdde5e12207b0800a91307803c
alinashveida <alinashveida@gmail.com>
2021-04-11 00:51:33 +0300

    2

--- gallery.js
+++ gallery.js
@@ -42,6 +42,9 @@ galleryList.addEventListener('click', onGalleryClick);
 
 const images = document.querySelectorAll('.gallery__image');
 
+const imageOverlay = document.querySelector('.lightbox__image');
+console.log(imageOverlay);
+
 
 
 function onGalleryClick (event) {
@@ -53,27 +56,16 @@ function onGalleryClick (event) {
   }
    lightbox.classList.add("is-open");
 
-//    const imageSource = target.dataset.source;
-//    console.log(imageSource);
+   imageOverlay.src = target.dataset.source;
+   imageOverlay.alt = target.alt;
 
-   
-//    console.log(target.src);
-
-   target.src = target.dataset.source;
-
-//    target.src = imageSource;
-  
-
-  
-
-   
 
   // console.log(event.target.nodeName);
   // const target = event.currentTarget;
   // console.log(target);
 };
 
-
+//-------закриття модалки---------------------------------//
 const buttonClose = document.querySelector('.lightbox__button');
 console.log(buttonClose);
 
@@ -81,5 +73,64 @@ buttonClose.addEventListener('click', onButtonClose);
 
 function onButtonClose (event){
   lightbox.classList.remove("is-open");
-  
+  imageOverlay.src = "";
+  imageOverlay.alt = "";
+
+};
+
+//----------overlay-------
+const overlay = document.querySelector('.lightbox__overlay');
+
+overlay.addEventListener('click', onClickOverlay);
+
+function onClickOverlay(event){
+    lightbox.classList.remove("is-open");
+    imageOverlay.src = "";
+    imageOverlay.alt = "";
+};
+
+//----escape----
+window.addEventListener('keydown', onEscClose);
+
+function onEscClose(event){
+    if(event.key !== "Escape"){
+        return
+    }
+    if(lightbox.classList.contains("is-open")){
+        lightbox.classList.remove("is-open");
+        imageOverlay.src = "";
+        imageOverlay.alt = "";
+    }
+    
+};
+
+
+
+//------------
+
+window.addEventListener('keydown', onKeyDownGallery);
+
+let currentIndex = 1
+function onKeyDownGallery(event){
+    // if(event.code !=="ArrowRight" || event.code !== "ArrowLeft" ){
+    //     return
+    // }
+    if(event.code === "ArrowRight"){
+        console.log(currentIndex);
+        currentIndex +=1;
+    }
+
+    if(event.code === "ArrowLeft"){
+        currentIndex -=1;
+        console.log(currentIndex)
+    }
+
+    if(currentIndex < 0 || currentIndex > galleryItems.length){
+        currentIndex = 0
+    }
+    setModalImage(currentIndex);
+}
+
+function setModalImage(index){
+    console.log(galleryItems[index]);
 }
